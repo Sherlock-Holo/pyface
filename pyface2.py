9fb331d81f549472c178393b674f5171aab076d0
VampireSH <sherlockya@gmail.com>
2017-03-21 09:28:47 +0800

    there are some bugs

--- pyface2.py
+++ pyface2.py
@@ -1,14 +1,13 @@
-#1/usr/env python3
+#!/usr/bin/env python3
 
 import cv2
 import sys
-
 
 face_patterns = cv2.CascadeClassifier("/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml")
 sample_img = sys.argv[1]
 detect_img = sys.argv[2]
 sample = cv2.imread(sample_img)
-faces = face_patterns.detectMultiScale(sample, scaleFactor = 1.1, minNeighbors = 4, minSize = (100, 100))
+faces = face_patterns.detectMultiScale(sample, scaleFactor = 1.1, minNeighbors = 2, minSize = (100, 100))
 
 for x, y, w, h in faces:
     cv2.rectangle(sample, (x, y), (x + w, y + h), (0, 255), 2)
