ecfc8680693815a6c85839c2145bee5299c1efe1
VampireSH <sherlockya@gmail.com>
2017-03-22 19:38:36 +0800

    reefine interpolation

--- pyface2.py
+++ pyface2.py
@@ -7,7 +7,7 @@ face_patterns = cv2.CascadeClassifier("/usr/share/opencv/haarcascades/haarcascad
 sample_img = sys.argv[1]
 #detect_img = sys.argv[2]
 sample = cv2.imread(sample_img)
-faces = face_patterns.detectMultiScale(sample, scaleFactor = 1.2, minNeighbors = 5, minSize = (100, 100))
+faces = face_patterns.detectMultiScale(sample, scaleFactor = 1.2, minNeighbors = 2, minSize = (100, 100))
 
 for x, y, w, h in faces:
     cv2.rectangle(sample, (x, y), (x + w, y + h), (0, 255), 2)
@@ -15,7 +15,7 @@ for x, y, w, h in faces:
 #cv2.imwrite(detect_img, sample)
 
 while sample.shape[0] >= 1920 or sample.shape[1] >= 1080:
-    sample = cv2.resize(sample, None, fx = 0.5, fy = 0.5, interpolation = cv2.INTER_CUBIC)
+    sample = cv2.resize(sample, None, fx = 0.5, fy = 0.5, interpolation = cv2.INTER_AREA)
 
 cv2.imshow('Face', sample)
 cv2.waitKey(0)
